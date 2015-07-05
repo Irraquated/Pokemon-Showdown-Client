33e4b3165b52fb4d85afeb576f322517bbf40d26
Irraquated <hookerjackson22@gmail.com>
2015-07-05 19:26:22 +1000

    Change /buttonkick to /buttonwarn
    (Also keeping /buttonkick as an alias) Makes the command much more understandable.

--- js/client-chat.js
+++ js/client-chat.js
@@ -826,11 +826,12 @@
 			case 'buttonunmute':
 				this.send('/unmute ' + target);
 				return false;
-
+			
 			case 'buttonkick':
+			case 'buttonwarn':
 				var self = this;
-				app.addPopupPrompt("Why do you wish to kick this user?", "Kick user", function(reason) {
-					self.send('/kick ' + toName(target) + ', ' + (reason || ''));
+				app.addPopupPrompt("Why do you wish to warn this user?", "Warn user", function(reason) {
+					self.send('/warn ' + toName(target) + ', ' + (reason || ''));
 				});
 				return false;
 
